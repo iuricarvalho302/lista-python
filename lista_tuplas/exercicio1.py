be1ac7278fcd676c81d2e7f4344651f5e33877b7
iuricarvalho302 <iuricarvalho302@gmail.com>
2023-06-01 22:38:34 -0300

    py

---
+++ lista_tuplas/exercicio1.py
@@ -0,0 +1,11 @@
+pessoa = {
+    "primeiro_nome": "Iuri",
+    "sobrenome": "Carvalho",
+    "idade": 18,
+    "cidade": "João Pessoa"
+}
+
+print("Primeiro Nome:", pessoa["primeiro_nome"])
+print("Sobrenome:", pessoa["sobrenome"])
+print("Idade:", pessoa["idade"])
+print("Cidade:", pessoa["cidade"])
---
+++ lista_tuplas/exercicio2.py
@@ -0,0 +1,10 @@
+numeros_favoritos = {
+    "Iuri": 21,
+    "Augusto": 123,
+    "Jerhan": 22,
+    "Kaio": 13,
+    "Rodrigo": 98,
+}
+
+for pessoa, numero in numeros_favoritos.items():
+    print(pessoa + ":", numero)
